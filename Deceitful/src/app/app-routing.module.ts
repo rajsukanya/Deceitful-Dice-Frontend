ac7725c5fc94b1f3bd65a42fd4eef905b05e8376
rajsukanya <sukanyaraj1014@yahoo.com>
2021-02-23 20:23:58 -0500

    finished landing page

--- Deceitful/src/app/app-routing.module.ts
+++ Deceitful/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GameComponent } from './game/game.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ScoreComponent } from './score/score.component';
 
 const routes: Routes = [
-  {path:'', component: LoginComponent },
-  {path:'login', component: LoginComponent},
+  {path:'', component: HomeComponent},
+  {path:'home', component: HomeComponent},
   {path:'game', component: GameComponent},
   {path:'score', component: ScoreComponent}
 ];
